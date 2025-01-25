0196e7f476b8f8c1671d8622130d8ac68c2ca585
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-01-25 12:24:47 +0200

    add task3

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,66 +6,102 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp10
 {
-    public class Passport
+    public class Animal
     {
-        public string PassportNumber { get; set; }
-        public string FullName { get; set; }
-        public string DateOfBirth { get; set; }
-        public string Citizenship { get; set; }
-        public Passport(string passportNumber, string fullName, string dateOfBirth, string citizenship)
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Habitat { get; set; }
+        public Animal(string name, int age, string habitat)
         {
-            PassportNumber = passportNumber;
-            FullName = fullName;
-            DateOfBirth = dateOfBirth;
-            Citizenship = citizenship;
+            Name = name;
+            Age = age;
+            Habitat = habitat;
         }
         public virtual void DisplayInfo()
         {
-            Console.WriteLine("Passport Information:");
-            Console.WriteLine($"Passport Number: {PassportNumber}");
-            Console.WriteLine($"Full Name: {FullName}");
-            Console.WriteLine($"Date of Birth: {DateOfBirth}");
-            Console.WriteLine($"Citizenship: {Citizenship}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age} years");
+            Console.WriteLine($"Habitat: {Habitat}");
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Name} makes a sound.");
         }
     }
-    public class ForeignPassport : Passport
+    public class Tiger : Animal
     {
-        public string ForeignPassportNumber { get; set; }
-        public List<string> Visas { get; set; }
-        public ForeignPassport(string passportNumber, string fullName, string dateOfBirth, string citizenship, string foreignPassportNumber)
-            : base(passportNumber, fullName, dateOfBirth, citizenship)
+        public string FurColor { get; set; }
+        public Tiger(string name, int age, string habitat, string furColor)
+            : base(name, age, habitat)
         {
-            ForeignPassportNumber = foreignPassportNumber;
-            Visas = new List<string>();
+            FurColor = furColor;
         }
-        public void AddVisa(string visa)
+
+        public override void DisplayInfo()
         {
-            Visas.Add(visa);
+            base.DisplayInfo();
+            Console.WriteLine($"Fur Color: {FurColor}");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} roars!");
+        }
+    }
+    public class Crocodile : Animal
+    {
+        public int Length { get; set; }
+        public Crocodile(string name, int age, string habitat, int length)
+            : base(name, age, habitat)
+        {
+            Length = length;
         }
         public override void DisplayInfo()
         {
             base.DisplayInfo();
-            Console.WriteLine($"Foreign Passport Number: {ForeignPassportNumber}");
-            Console.WriteLine("Visas:");
-            foreach (var visa in Visas)
-            {
-                Console.WriteLine($" - {visa}");
-            }
+            Console.WriteLine($"Length: {Length} meters");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} growls!");
+        }
+    }
+    public class Kangaroo : Animal
+    {
+        public double JumpHeight { get; set; }
+        public Kangaroo(string name, int age, string habitat, double jumpHeight)
+            : base(name, age, habitat)
+        {
+            JumpHeight = jumpHeight;
+        }
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Jump Height: {JumpHeight} meters");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} makes a boing sound!");
         }
     }
     public class Program
     {
-        public static void Main(string[] args) { 
-
-            Passport passport = new Passport("AB123456", "Ivan Petrov", "01/01/1990", "Ukraine");
-            ForeignPassport foreignPassport = new ForeignPassport("CD789012", "Maria Ivanova", "15/06/1985", "Russia", "FX123456");
-            foreignPassport.AddVisa("Schengen Visa");
-            foreignPassport.AddVisa("USA Visa");
-            Console.WriteLine("Passport Info:");
-            passport.DisplayInfo();
+        public static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger("Sher Khan", 5, "Jungle", "Orange with black stripes");
+            Crocodile crocodile = new Crocodile("Gator", 8, "Swamp", 5);
+            Kangaroo kangaroo = new Kangaroo("Kenny", 3, "Australian Outback", 3.5);
+            Console.WriteLine("Tiger Info:");
+            tiger.DisplayInfo();
+            tiger.MakeSound();
             Console.WriteLine();
-            Console.WriteLine("Foreign Passport Info:");
-            foreignPassport.DisplayInfo();
+            Console.WriteLine("Crocodile Info:");
+            crocodile.DisplayInfo();
+            crocodile.MakeSound();
+            Console.WriteLine();
+            Console.WriteLine("Kangaroo Info:");
+            kangaroo.DisplayInfo();
+            kangaroo.MakeSound();
         }
     }
 }
